cafff9515963e2f8dabde8517d6b2d80dfabc296
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-04 18:29:31 +0530

    debugElement vs nativeElement

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('HeroComponent (shallow tests)', () => {
     let fixture : ComponentFixture<HeroComponent>;
@@ -24,6 +25,12 @@ describe('HeroComponent (shallow tests)', () => {
     it('should render the hero name in an anchor tag', () => {
         fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
         fixture.detectChanges();
-        expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+
+        //deA is debug element form Anchor tag
+        const deA = fixture.debugElement.query(By.css('a'));
+        expect(deA.nativeElement.textContent).toContain('SuperDude');
+
+        //or below can also be used using nativeElement
+        //expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
     });
 });
