fb4acaeceefca9e5046aa30b62f85a1a37c5d183
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-04 16:40:11 +0530

    checking if a service function is called with correct paramter or not

--- src/app/heroes/heros.component.spec.ts
+++ src/app/heroes/heros.component.spec.ts
@@ -26,11 +26,11 @@ describe('HeroesComponent', () => {
             expect(component.heroes.length).toBe(2);
         });
 
-        it('should call deleteHero', () => {
+        it('should call deleteHero with correct hero', () => {
             mockHeroService.deleteHero.and.returnValue(of(true));
             component.heroes = HEROES;
             component.delete(HEROES[2]);
-            expect(mockHeroService.deleteHero).toHaveBeenCalled();
+            expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
         });
     });
 
