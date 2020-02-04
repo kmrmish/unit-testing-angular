63355e223b1817499db2a9787fdffe7d4f5d5a76
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-04 16:26:31 +0530

    testing if the service method was called or not

--- src/app/heroes/heros.component.spec.ts
+++ src/app/heroes/heros.component.spec.ts
@@ -15,6 +15,7 @@ describe('HeroesComponent', () => {
         mockHeroService = jasmine.createSpyObj(['getHeroes','addHero','deleteHero']);
         component = new HeroesComponent(mockHeroService);
     });
+
     describe('delete', () => {
         it('should remove the indicated hero from the heroes list', () => {
             /* of: rxjs function to create an observable having value 'true' in this case */
@@ -24,5 +25,14 @@ describe('HeroesComponent', () => {
 
             expect(component.heroes.length).toBe(2);
         });
+
+        it('should call deleteHero', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true));
+            component.heroes = HEROES;
+            component.delete(HEROES[2]);
+            expect(mockHeroService.deleteHero).toHaveBeenCalled();
+        });
     });
+
+    
 });
