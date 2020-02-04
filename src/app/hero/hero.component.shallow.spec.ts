d14384b5c057729a6c980fdcefb1cec4f4672833
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-04 17:53:32 +0530

    adding --source-map=false in test command, using fixtures, TestBed and NO_ERRORS_SCHEMA

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { HeroComponent } from "./hero.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+
+describe('HeroComponent (shallow tests)', () => {
+    let fixture : ComponentFixture<HeroComponent>;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations : [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA] 
+            /* NO_ERRORS_SCHEMA means, do not report 
+            error for any unknown variable on template */
+        });
+       
+        fixture = TestBed.createComponent(HeroComponent);
+    });
+
+    it('should have the correct hero', () => {
+        fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
+
+        expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+    });
+});
