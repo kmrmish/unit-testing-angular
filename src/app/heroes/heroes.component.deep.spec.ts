f31db0096c9c709c134989a3205004e369b9ed72
Mukesh Kumar <mukesh.kumar@taazaa.com>
2020-02-05 12:32:58 +0530

    deep test: checking data send from parent component to child components is correctly sent or not

---
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,56 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HeroesComponent } from "./heroes.component";
+import { NO_ERRORS_SCHEMA, Component, Input } from "@angular/core";
+import { HeroService } from "../hero.service";
+import { of } from "rxjs";
+import { Hero } from "../hero";
+import { By } from "@angular/platform-browser";
+import { HeroComponent } from "../hero/hero.component";
+
+describe('HeroesComponent (deept tests)', () => {
+    let fixture: ComponentFixture<HeroesComponent>;
+    let mockHeroService;
+    let HEROES;
+    
+
+
+    beforeEach(() => {
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 2, name: 'Wonderful Woman', strength: 24},
+            {id: 3, name: 'SuperDude', strength: 55}
+        ];
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+        
+        TestBed.configureTestingModule({
+            declarations: [
+                HeroesComponent,
+                HeroComponent
+            ],
+            providers: [
+               {provide: HeroService, useValue: mockHeroService} 
+            ],
+            schemas : [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(HeroesComponent);
+        
+    });
+
+    it('should render each hero as a hero component', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+        /* run ngOnInit */
+        fixture.detectChanges();
+
+        let heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+        expect(heroComponentDEs.length).toEqual(3);
+
+        // expect(heroComponentDEs[0].componentInstance.hero.name).toEqual('SpiderDude');
+        // expect(heroComponentDEs[1].componentInstance.hero.name).toEqual('Wonderful Woman');
+        // expect(heroComponentDEs[2].componentInstance.hero.name).toEqual('SuperDude');
+
+        for(let i = 0; i < heroComponentDEs.length; i++){
+            expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
+        }
+    });
+});
